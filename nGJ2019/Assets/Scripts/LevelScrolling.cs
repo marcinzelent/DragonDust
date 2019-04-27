8973398675a1ea012da316f791448d9437fc8528
marcinzelent <zelent.marcin@gmail.com>
2019-04-27 20:21:50 +0200

    Added destruction of obstacles

--- nGJ2019/Assets/Scripts/LevelScrolling.cs
+++ nGJ2019/Assets/Scripts/LevelScrolling.cs
@@ -4,66 +4,76 @@ using UnityEngine;
 
 public class LevelScrolling : MonoBehaviour
 {
-  public int UpdateRate = -5;
-  public List<Transform> Obstacles;
-  public Transform Background;
+    public int UpdateRate = -5;
+    public List<GameObject> Obstacles;
+    public Transform Background;
 
-  private Vector3 initialBgPos;
-  private float[] backgroundSize;
+    private Vector3 initialBgPos;
+    private float[] backgroundSize;
 
-  // Start is called before the first frame update
-  void Start()
-  {
-    ResizeBackground();
-  }
-
-  // Update is called once per frame
-  void Update()
-  {
-
-  }
-
-  void FixedUpdate()
-  {
-    MoveBackground();
-    MoveObstacles();
-  }
-
-  private void ResizeBackground()
-  {
-    var sr = Background.GetComponent<SpriteRenderer>();
-    if (sr == null) return;
-
-    Background.localScale = new Vector3(1, 1, 1);
-
-    var width = sr.sprite.bounds.size.x;
-    var height = sr.sprite.bounds.size.y;
-
-    var worldScreenHeight = Camera.main.orthographicSize * 2.0;
-    var worldScreenWidth = worldScreenHeight / Screen.height * Screen.width;
-
-    var finalHeight = (float)(worldScreenHeight / height);
-    var finalWidth = (float)(worldScreenWidth / width);
-
-    Background.localScale = new Vector3(finalHeight, finalHeight, 1);
-
-    var viewportX = Camera.main.ViewportToWorldPoint(new Vector3(0, 1, 0)).x;
-    Background.position = new Vector3(viewportX * -1, 0, 0);
-    initialBgPos = Background.position;
-  }
-
-  private void MoveObstacles()
-  {
-    foreach (var o in Obstacles)
+    // Start is called before the first frame update
+    void Start()
     {
-        o.Translate(0.01f * UpdateRate, 0, 0);
+        ResizeBackground();
     }
-  }
 
-  private void MoveBackground()
-  {
-    if (Background.position.x > -initialBgPos.x)
-      Background.Translate(0.01f * UpdateRate, 0, 0);
-    else Background.position = initialBgPos;
-  }
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void FixedUpdate()
+    {
+        MoveBackground();
+        MoveObstacles();
+        DestroyObstacles();
+    }
+
+    void ResizeBackground()
+    {
+        var sr = Background.GetComponent<SpriteRenderer>();
+        if (sr == null) return;
+
+        Background.localScale = new Vector3(1, 1, 1);
+
+        var width = sr.sprite.bounds.size.x;
+        var height = sr.sprite.bounds.size.y;
+
+        var worldScreenHeight = Camera.main.orthographicSize * 2.0;
+        var worldScreenWidth = worldScreenHeight / Screen.height * Screen.width;
+
+        var finalHeight = (float)(worldScreenHeight / height);
+        var finalWidth = (float)(worldScreenWidth / width);
+
+        Background.localScale = new Vector3(finalHeight, finalHeight, 1);
+
+        var viewportX = Camera.main.ViewportToWorldPoint(new Vector3(0, 1, 0)).x;
+        Background.position = new Vector3(viewportX * -1, 0, 0);
+        initialBgPos = Background.position;
+    }
+
+    void MoveObstacles()
+    {
+        for (int i = 0; i < Obstacles.Count; i++)
+        {
+            Obstacles[i].transform.Translate(0.01f * UpdateRate, 0, 0);
+        }
+    }
+
+    void DestroyObstacles()
+    {
+        if (Obstacles[0].transform.position.x < -30)
+        {
+            Destroy(Obstacles[0]);
+            Obstacles.RemoveAt(0);
+        }
+    }
+
+    void MoveBackground()
+    {
+        if (Background.position.x > -initialBgPos.x)
+            Background.Translate(0.01f * UpdateRate, 0, 0);
+        else Background.position = initialBgPos;
+    }
 }
